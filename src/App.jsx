e7334762350a57eee19a48119fa6cfb8eda305f6
RonakAgal <144449528+RonakAgal@users.noreply.github.com>
2025-10-07 10:20:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,3 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import ProductList from "./components/ProductList";
-// import Cart from "./components/Cart";
-// import "./App.css";
-
-// // const userId = "user123"; // Example user
-const API = "https://cart-backend-xea4.onrender.com";
-// const userId = "anonymousUser";
-
-
-// function App() {
-//   const [products, setProducts] = useState([]);
-//   const [cart, setCart] = useState({ subtotal: 0, tax: 0, total: 0 });
-
-//   useEffect(() => {
-//     const fetchProducts = async () => {
-//       const res = await axios.get(`${API}/products`);
-//       setProducts(res.data);
-//     };
-//     fetchProducts();
-//     fetchCartTotal();
-//   }, []);
-
-//   const addToCart = async (productId) => {
-//     await axios.post(`${API}/cart/add`, {
-//       userId,
-//       productId,
-//       quantity: 1,
-//     });
-//     fetchCartTotal();
-//   };
-
-//   const fetchCartTotal = async () => {
-//     const res = await axios.get(`${API}/cart/total`, {
-//       headers: { userId },
-//     });
-//     setCart(res.data);
-//   };
-
-//   return (
-//     <div className="container">
-//       <h1>Products</h1>
-//       <ProductList products={products} addToCart={addToCart} />
-//       <Cart cart={cart} />
-//     </div>
-//   );
-// }
-
-// export default App;
   
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -109,3 +59,8 @@ function App() {
 }
 
 export default App;
+
+
+  
+
+ 
